8120c7ebc2cfb5a7ff1d7e734bb2e1c73a134057
liangweiguo <2815171417@qq.com>
2025-05-12 15:31:14 +0800

    去除eslintignore警告

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect, useLayoutEffect } from 'react';
-import Map from './map';
+import { Map } from './map';
 import ToolContext from './ToolContext'
 
 
 export default function Home(props) {
     const [user, setUser] = useState();
+    const [map, setMap] = useState();
 
     return (
         <ToolContext.Provider value={{
             map, setMap,
         }}>
-            <div className="page-home">
+            <div className="Home">
                 <Map />
             </div>
         </ToolContext.Provider>
--- src/map/Map.js
+++ src/map/Map.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-export default function BaseMap(props) {
+export default function Map(props) {
 
 	return (
         <div>111111</div>
